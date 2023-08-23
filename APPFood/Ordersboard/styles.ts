075481e766701b3fa60dafda6128398f5796e8ce
João Marques <51685504+marquesrx@users.noreply.github.com>
2023-08-23 11:01:35 -0300

    Create styles.ts

---
+++ APPFood/Ordersboard/styles.ts
@@ -0,0 +1,20 @@
+import { styled } from "styled-components";
+
+export const Board = styled.div`
+    padding: 10;
+    border: 1px solid rgb(284, 204, 204, 0.4);
+    border-radius: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+
+    header{
+        padding: 8px;
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        
+    }
+`;
