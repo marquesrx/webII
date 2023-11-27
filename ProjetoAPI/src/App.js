f7b212570a876db58d50e45f44caf62ed1331115
João Marques <51685504+marquesrx@users.noreply.github.com>
2023-11-27 10:50:23 -0300

    Add files via upload

---
+++ ProjetoAPI/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ApiComponent from './ApiComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ApiComponent />
+    </div>
+  );
+}
+
+export default App;
